6e91de95cce8a2065435e880d24847e421cc74ee
mayank-725 <mayank.sachdeva725@gmail.com>
2020-03-28 00:27:21 +0530

    [Mayank] Refactor. adding Interface IManager to Manager Project Module

---
+++ Manager/IManager.cs
@@ -0,0 +1,16 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manager
+{
+    public interface IManager
+    {
+        EmployeeModel GetEmployee(int id);
+        IEnumerable<EmployeeModel> GetAllEmployees();
+        EmployeeModel AddEmployee(EmployeeModel employee);
+        EmployeeModel UpdateEmployee(EmployeeModel employeeChanges);
+        EmployeeModel DeleteEmployee(int id);
+    }
+}
