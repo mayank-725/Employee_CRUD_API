7bdaf3fa6c37b2051c01f721fce885ddfdac10a6
mayank-725 <mayank.sachdeva725@gmail.com>
2020-03-27 23:32:51 +0530

    [Mayank] Refactor. adding EmployyeRepoImpl file to repository

--- Repository/IEmployeeRepo.cs
+++ Repository/IEmployeeRepo.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Repository
 {
-    interface IEmployeeRepo
+    public  interface IEmployeeRepo
     {
+        EmployeeModel GetEmployee(int id);
+        IEnumerable<EmployeeModel> GetAllEmployees();
+        Task<int> AddEmployee(EmployeeModel employee);
+        Task<int> UpdateEmployee(EmployeeModel employeeChanges);
+        EmployeeModel DeleteEmployee(int id);
     }
 }
