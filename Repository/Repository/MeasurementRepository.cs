5a6e5a9da28d4c0c71217535895d65d299f8a812
mayank-725 <mayank.sachdeva725@gmail.com>
2020-04-21 01:58:08 +0530

    [Mayank]Add. adding measurment conversion to repository project

---
+++ Repository/Repository/MeasurementRepository.cs
@@ -0,0 +1,41 @@
+﻿using Model;
+using Repository.RepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class MeasurementRepository : IMeasurementRepository
+    {
+        public decimal CentimetertoMeter(MeasuringUnits value)
+        {
+            return value.Centimeter / 100;
+        }
+
+        public decimal FeettoInch(MeasuringUnits value)
+        {
+            return value.Centimeter / 100;
+        }
+
+        public decimal GramsToKilograms(MeasuringUnits value)
+        {
+            return value.Gram / 1000;
+        }
+
+        public decimal InchToFeet(MeasuringUnits value)
+        {
+            return value.Inch / 12;
+        }
+
+        public decimal KilogramsToGrams(MeasuringUnits value)
+        {
+            return value.KiloGram * 1000;
+        }
+
+        public decimal MetertoCentimeter(MeasuringUnits value)
+        {
+            return value.Meter * 100;
+        }
+    }
+}
---
+++ Repository/RepositoryInterface/IMeasurementRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.RepositoryInterface
+{
+    class IMeasurementRepository
+    {
+        decimal FeettoInch(MeasuringUnits value);
+
+        decimal InchToFeet(Conversion value);
+
+        decimal MetertoCentimeter(Conversion value);
+
+        decimal CentimetertoMeter(Conversion value);
+
+        decimal KgtoGm(Conversion value);
+
+        decimal GmtoKg(Conversion value);
+    }
+}
